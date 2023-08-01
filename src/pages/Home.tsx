4943897248ce006f52a30f98e6031878d36bac3f
JeongTaeUk <73880776+peacepiece7@users.noreply.github.com>
2023-08-01 18:57:50 +0900

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,9 @@
 import React from 'react'
 export default function Home() {
-  return <div>Home(landing) page</div>
+  return <div>Home(landing) page
+        <a href='/calendar/2023/07/01'>calendar</a>
+      <a href='/login/test'>test login</a>
+      <a href='/sample'>sample</a>
+      <a href='/sample/optimistic'>sample</a>
+  </div>
 }
